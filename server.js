b41814608f507fdab6af7d672b4a7cb9ba2dcfad
o.kiryushin <o.kiryushin@mightycall.ru>
2024-03-05 17:55:44 +0300

    init

---
+++ server.js
@@ -0,0 +1,20 @@
+﻿const express = require('express');
+
+const PORT = 8080;
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/asterisk-ws-api/api/CallWebHook", function (req, res) {
+	console.log(req.body);
+	res.send({ result: 0 });
+});
+
+app.get("/", (req, res) => {
+	res.send("it works");
+});
+
+app.listen(PORT, () => {
+	console.log(`Сервер запущен на порту ${PORT}\n`);
+});
